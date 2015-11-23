da8dfdc742644c931afb15904b41bb0862294e51
qianfeng <qianfeng@woqunianmailegebiao.local>
2015-11-23 14:45:44 +0800

    v1

---
+++ tianbinVersion2/tianbinVersion2Tests/tianbinVersion2Tests.m
@@ -0,0 +1,40 @@
+//
+//  tianbinVersion2Tests.m
+//  tianbinVersion2Tests
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 TB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface tianbinVersion2Tests : XCTestCase
+
+@end
+
+@implementation tianbinVersion2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
