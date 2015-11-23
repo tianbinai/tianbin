e11339722f675f9ba350da7852076caa7ed08711
qianfeng <qianfeng@woqunianmailegebiao.local>
2015-11-23 14:48:21 +0800

    meinv1

---
+++ meinv1/meinv1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  meinv1
+//
+//  Created by qianfeng on 15/11/23.
+//  Copyright (c) 2015年 TB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
